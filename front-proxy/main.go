d71f11bf8eafbff78d6710a14995c4a2b2a472d6
anysz <richiisei@gmail.com>
2019-07-04 08:46:50 +0200

    Additional logging & Node tracking

--- front-proxy/main.go
+++ front-proxy/main.go
@@ -2,11 +2,14 @@ package main
 // Author: Anysz
 import (
     "fmt"
+    "log"
     "os"
     "io"
     "flag"
+    "net"
     "net/http"
     "net/url"
+    "context"
     _ "net/http/httputil"
 )
 func main() {
@@ -21,15 +24,50 @@ func main() {
     if err != nil { panic(err) }
     destUrl, err := url.Parse(*destPtr)
     if err != nil { panic(err) }
-    http.DefaultTransport = &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
 
-    //fmt.Printf("%#+v\n" , *destUrl)
+    makeClient := func() (cl http.Client, resolv_ip string) {
+        ips, err := net.LookupIP(proxyUrl.Hostname())
+        if err != nil { panic(err) }
+	proxyStruct := &url.URL{}
+	if len(ips) > 0 {
+          resolv_ip = fmt.Sprintf("%s", ips[0])
+          proxyStruct = &url.URL{
+            Scheme: proxyUrl.Scheme,
+            Opaque : proxyUrl.Opaque,
+            User: proxyUrl.User,
+            Host: fmt.Sprintf("%s:%s", resolv_ip, proxyUrl.Port()),
+            Path: proxyUrl.Path,
+            RawPath: proxyUrl.RawPath,
+            ForceQuery: proxyUrl.ForceQuery,
+            RawQuery: proxyUrl.RawQuery,
+            Fragment: proxyUrl.Fragment,
+          }
+        }else{
+          resolv_ip = "no_scope"
+          proxyStruct = proxyUrl
+        }
+        cl = http.Client{
+            Transport: &http.Transport{
+                Proxy: http.ProxyURL(proxyStruct),
+                DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+                    conn, err := net.Dial(network, addr)
+                    if err == nil {
+                       log.Println("Addr", addr, "Ip:", conn.RemoteAddr())
+                    }else{ log.Println(err) }
+                       return conn, err
+                    },
+            },
+        }
+	return
+    }
 
-    _ = destUrl
+    _=destUrl
 
     http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-        rr, _ := http.NewRequest(r.Method, *destPtr + r.RequestURI, r.Body)
-        res, err := http.DefaultClient.Do(rr)
+        rr, err  := http.NewRequest(r.Method, *destPtr + r.RequestURI, r.Body)
+        if err != nil { fmt.Fprintf(w, "Err %+v\n", err.Error()) }
+        cl, proxy_node := makeClient()
+        res, err := cl.Do(rr)
         if err != nil {
             fmt.Fprintf(w, "Err %#+v\n", err.Error())
         }else{
@@ -37,6 +75,7 @@ func main() {
             for k, v := range res.Header {
                 w.Header().Set(k, v[0])
             }
+            w.Header().Set("Node-IP", proxy_node)
             defer res.Body.Close()
             io.Copy(w, res.Body)
         }
